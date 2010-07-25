48cc0b1076c3bc9f5a99155e016a03152ff5551b
kpollard <kpollard@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-07-25 20:47:30 +0000

    Removed library() statements in test.R and EBMTP.R, since don't require cluster packages.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/multtest@48411 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/EBMTP.R
+++ R/EBMTP.R
@@ -281,7 +281,7 @@ EBMTP<-function(X,W=NULL,Y=NULL,Z=NULL,Z.incl=NULL,Z.test=NULL,na.rm=TRUE,test="
     ## Check installation of packages
       have_snow <- qRequire("snow")
       if(!have_snow) stop("The package snow is required to use a cluster. Either snow is not installed or it is not in the standard library location.")
-      library("snow")  #use library() since several functions are needed
+      #library("snow")  #use library() since several functions are needed
       if (is.null(type))
          stop("Must specify type argument to use a cluster. Alternatively, provide a cluster object as the argument to cluster.")
       if (type=="SOCK")
@@ -291,12 +291,12 @@ EBMTP<-function(X,W=NULL,Y=NULL,Z=NULL,Z.incl=NULL,Z.test=NULL,na.rm=TRUE,test="
       else if (type=="MPI"){
          have_rmpi <- qRequire("Rmpi")
          if(!have_rmpi) stop("The package Rmpi is required for the specified type. Either Rmpi is not installed or it is not in the standard library location.")
-         library("Rmpi")
+         #library("Rmpi")
        }
       else if (type=="PVM"){
          have_rpvm <- qRequire("rpvm")
          if(!have_rpvm) stop("The package rpvm is required for the specified type. Either rpvm is not installed or it is not in the standard library location.")
-         library("rpvm")
+         #library("rpvm")
       }
       cluster <- makeCluster(cluster, type)
       clusterEvalQ(cluster, {library(Biobase); library(multtest)})
